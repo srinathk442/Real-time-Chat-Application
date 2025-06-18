b41878436ae569eddd6d94ca01f737f7f2c0466b
Dev Ops <srinathk442@gmail.com>
2025-06-18 15:50:28 +0530

    Updated

---
+++ src/test/java/com/chat/realtime_chat/SomeTest.java
@@ -0,0 +1,12 @@
+package com.chat.realtime_chat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeTest {
+    @Test
+    void testBasicTruth() {
+        assertTrue(true);
+    }
+}
